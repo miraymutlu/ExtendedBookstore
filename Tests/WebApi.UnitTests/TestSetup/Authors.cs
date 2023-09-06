04182279e76b5b0e86da17d38641a19715bb70e2
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-09-06 16:30:29 +0300

    Authors added for tests

---
+++ Tests/WebApi.UnitTests/TestSetup/Authors.cs
@@ -0,0 +1,30 @@
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace WebApi.UnitTests.TestSetup;
+
+public class Authors
+{
+    public static void AddAuthors(BookstoreDbContext context)
+    {
+        context.Authors.AddRange(
+            new Author
+            {
+                Name = "George",
+                Surname = "Saunders",
+                Birthday = new DateTime(1970,01,06)
+            },
+            new Author
+            {
+                Name = "Jean Louis",
+                Surname = "Fourniere",
+                Birthday = new DateTime(1938,12,16)
+            },
+            new Author
+            {
+                Name = "Haruki",
+                Surname = "Murakami",
+                Birthday = new DateTime(1956,06,24)
+            });
+    }
+}
