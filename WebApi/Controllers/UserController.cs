12260626d9dc878af37e5ff1017ce62fb198fa72
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-09-09 13:19:35 +0300

    UserController.cs created

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DBOperations;
 
 namespace WebApi.Controllers;
 
@@ -6,5 +8,23 @@ namespace WebApi.Controllers;
 [Route("[controller]s")]
 public class UserController : ControllerBase
 {
-    
+    private readonly IBookStoreDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly IConfiguration _configuration;
+    public UserController(IBookStoreDbContext context, IMapper mapper, IConfiguration configuration)
+    {
+        _context = context;
+        _mapper = mapper;
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateUserModel newUser)
+    {
+        CreateUserCommand command = new CreateUserCommand(_context, _mapper);
+        command.Model = newUser;
+        command.Handle();
+
+        return Ok();
+    }
 }
