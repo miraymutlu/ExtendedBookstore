ee8fb38baa5491d960d6227a51ba7f41cf837ebc
Miray Mutlu <miray.mutlu@ozu.edu.tr>
2023-09-09 13:38:33 +0300

    Token.cs added

---
+++ WebApi/Application/UserOperations/Commands/CreateTokenCommand/CreateTokenCommand.cs
@@ -0,0 +1,44 @@
+using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using WebApi.DBOperations;
+
+namespace WebApi.Application.UserOperations.Commands.CreateTokenCommand;
+
+public class CreateTokenCommand
+{
+    public CreateTokenModel Model { get; set; }
+    private readonly IBookStoreDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly IConfiguration _configuration;
+
+    public CreateTokenCommand(IBookStoreDbContext dbContext, IMapper mapper, IConfiguration configuration)
+    {
+        _context = dbContext;
+        _mapper = mapper;
+        _configuration = configuration;
+    }
+
+    public Token Handle()
+    {
+        var user = _context.Users.FirstOrDefault(u => u.Email == Model.Email && u.Password == Model.Password);
+        if (user is not null)
+        {
+            // Create Token
+            TokenHandler tokenHandler = new(_configuration);
+            Token token = tokenHandler.CreateAccessToken(user);
+
+            user.RefreshToken = token.RefreshToken;
+            user.RefreshTokenExpireDate = token.Expiration.AddMinutes(5);
+            _context.SaveChanges();
+            return token;
+        }
+        else
+            throw new InvalidOperationException("Wrong username and password!");
+    }
+
+    public class CreateTokenModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
